88bbfd90b46f93d7f971f158af8b6603184dda0a
kowshik567 <kowshik567@gmail.com>
2026-03-27 09:23:00 +0530

    UC8: Filter Passenger Bogies Using Streams

--- App/src/Main.java
+++ App/src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class UseCase8TrainConsistMgmt {
+public class Main {
 
     // Reusing Bogie model (same as UC7)
     static class Bogie {
